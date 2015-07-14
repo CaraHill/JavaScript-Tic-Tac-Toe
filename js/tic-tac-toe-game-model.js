6b418487dd60fe8587007e3e33de7bffda68edbb
Cara Hill <chilly23@gmail.com>
2015-07-14 15:12:12 +1200

    Created JS folder and put all js files in it.

--- js/tic-tac-toe-game-model.js
+++ js/tic-tac-toe-game-model.js
@@ -2,7 +2,7 @@ function TicTacToeGameModel() {
 
 }
 
-TicTacToeGame.prototype = {
+TicTacToeGameModel.prototype = {
   placePiece: function(clickedSpace, currentPlayer) {
     if($(clickedSpace.target).data("type") == "space"){
       $(clickedSpace.target).text(currentPlayer);

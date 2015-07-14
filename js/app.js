894edd397c8709342e7fe6d00ce95136ffc194b2
Cara Hill <chilly23@gmail.com>
2015-07-14 15:39:46 +1200

    Added all the generate board logic into the ttt board view.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
 $(function() {
 
-  var rows = 3;
-  var columns = 3;
   var gamePieces = ["X", "0"]
+  var board = new TicTacToeBoardView();
+  var $board = board.generateBoard();
   var game = new TicTacToeGameModel();
-  var $board = generateBoard(rows, columns);
   var currentPlayer = 1;
   var index = 0;
-
-  function generateColumns (columns) {
-    var $column = $("<tr>");
-    var index = columns;
-    while(index-- > 0) {
-      $column.append("<td data-type=space>");
-    }
-    return $column;
-  }
-
-  function generateBoard (rows, columns) {
-    var $board = $("<table>");
-    var index = rows;
-    var $row;
-
-    while(index-- > 0) {
-      $row = generateColumns(columns);
-      $board.append($row);
-    }
-    return $board;
-  }
 
   $("#game-board").append($board);
 
--- js/views/tic-tac-toe-board-view.js
+++ js/views/tic-tac-toe-board-view.js
@@ -1,3 +1,27 @@
 function TicTacToeBoardView() {
+  this.row = 3;
+  this.column = 3;
+  this.gamePieces = ["X", "0"]
+}
 
+TicTacToeBoardView.prototype = {
+  generateColumns: function() {
+    var $column = $("<tr>");
+    var index = this.column;
+    while(index-- > 0) {
+      $column.append("<td data-type=space>");
+    }
+    return $column;
+  },
+  generateBoard: function() {
+    var $board = $("<table>");
+    var index = this.row;
+    var $row;
+
+    while(index-- > 0) {
+      $row = this.generateColumns(this.column);
+      $board.append($row);
+    }
+    return $board
+  }
 }
